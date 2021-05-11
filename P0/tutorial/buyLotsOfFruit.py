2d3f46a534b675c0338f52f33a3ecb137a168461
suuuuu017 <suuuuu017@ShiHes-MacBook-Pro.local>
2021-05-11 17:34:05 +0800

    modify tutorial to autograde

--- P0/tutorial/buyLotsOfFruit.py
+++ P0/tutorial/buyLotsOfFruit.py
@@ -1,17 +1,3 @@
-# buyLotsOfFruit.py
-# -----------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-
 """
 To run this script, type
 
@@ -36,6 +22,11 @@ def buyLotsOfFruit(orderList):
     """
     totalCost = 0.0
     "*** YOUR CODE HERE ***"
+    for key, val in orderList:
+        if key not in fruitPrices.keys():
+            print('No %s in shop' % key)
+        else:
+            totalCost = totalCost + val * fruitPrices[key]
     return totalCost
 
 
--- P0/tutorial/shopSmart.py
+++ P0/tutorial/shopSmart.py
@@ -1,17 +1,3 @@
-# shopSmart.py
-# ------------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-
 """
 Here's the intended output of this script, once you fill it in:
 
@@ -30,7 +16,8 @@ def shopSmart(orderList, fruitShops):
         fruitShops: List of FruitShops
     """
     "*** YOUR CODE HERE ***"
-    return None
+    costs = [shop.getPriceOfOrder(orderList) for shop in fruitShops]
+    return fruitShops[costs.index(min(costs))]
 
 
 if __name__ == '__main__':
